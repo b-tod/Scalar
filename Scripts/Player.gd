02236fdff841f97db8365c20f37b9c75c92a603d
Borislav Todorov <borislav.todorov@alsetstudio.com>
2023-11-06 19:53:56 +0100

    setup project directory structure

---
+++ Scripts/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+export var speed = 500
+
+var move_direction : Vector2
+
+func _ready():
+	pass
+
+func _process(delta):
+	
+	move_direction = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		move_direction += Vector2(0, -1)
+	if Input.is_action_pressed("ui_down"):
+		move_direction += Vector2(0, 1)
+	if Input.is_action_pressed("ui_left"):
+		move_direction += Vector2(-1, 0)
+	if Input.is_action_pressed("ui_right"):
+		move_direction += Vector2(1, 0)
+	
+	move_direction = move_direction.normalized()
+	position += move_direction * speed * delta
+
+func _input(event):
+	pass
