f782f657ea5a3f84bac50b24ad9a8aa5fcb24715
Filip Jovanovic <jovanovic16942@gmail.com>
2023-11-09 03:21:15 +0100

    Enemies can drop loot

--- Scripts/Loot.gd
+++ Scripts/Loot.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 export (Resource) var loot_path
 
+onready var source = $".."
+onready var world = $"../../.."
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,4 +15,20 @@ func _ready():
 
 func _notification(what):
 	if (what == NOTIFICATION_PREDELETE):
-		print("TODO: There should be loot...")
+		drop_loot()
+
+func drop_loot(min_drop = 1, max_drop = 5):
+	for i in range(0, int(rand_range(min_drop, max_drop))):
+		# TODO Move this to PickUp constructor/init method
+		var loot = loot_path.instance()
+		# Set position and direction
+		loot.global_position = source.global_position
+
+		var direction = (source.destination - source.global_position).normalized()
+
+		loot.direction = direction.rotated(rand_range(-PI/4, PI/4))
+		# Set velocity and rotation
+		loot.velocity = source.speed * rand_range(1/2, 3/2)
+		loot.spin = rand_range(-2*PI, 2*PI)
+
+		world.add_child(loot)
--- Scripts/PickUp.gd
+++ Scripts/PickUp.gd
@@ -3,11 +3,18 @@ class_name PickUp
 
 export var value = 10
 
+var velocity = 0
+var spin = 0
+export var direction : Vector2
+
+var timer
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	global_position += direction * velocity * delta
+	rotate(spin * delta)
